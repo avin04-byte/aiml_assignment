0b38c8c5b7da86131cb1168f3199954ab8cb46c5
avin04-byte <2311cs020032@mallareddyuniversity.ac.in>
2025-01-09 19:17:12 +0530

    Add files via upload

---
+++ Compute (1).py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+greet = lambda name : print(f"Good Morning {name}!")
+greet("Rick")
+
+
+# In[2]:
+
+
+#Product of three numbers
+product = lambda a,b,c : a*b*c
+product(2,4,7)
+
+
+# In[3]:
+
+
+even = lambda L : [x for x in L if x%2 == 0]
+my_list = [2,534,67,4,8,5,32,4,24]
+even(my_list)
+
+
+# In[ ]:
+
+
+
+
